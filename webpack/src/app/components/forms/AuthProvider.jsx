555f6a3a8f06d7e88fdd424d561a5c03831a61ac
kirill <bulychevkery@gmail.com>
2018-01-31 01:31:33 +0300

    auth

--- webpack/src/app/components/forms/AuthProvider.jsx
+++ webpack/src/app/components/forms/AuthProvider.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import {Icon, Tabs} from 'antd';
 import {Field, reduxForm} from 'redux-form'
-import {email, required} from "./Validation";
+import {email, inputName, maxLength, required} from "./Validation";
+import * as Path from '../../utils/RoutePath'
 
 const TabPane = Tabs.TabPane;
 
 const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
     <div>
-        <div>
-            <input {...input} placeholder={label} type={type}>
-            </input>
-            {touched && error ? <span className="line-error"/> : <span className="line"/>}
-            {touched && error ? <label className='error-label'>{error}</label> : undefined}
-        </div>
+        {touched && error ? <Icon type='exclamation-circle' className='error-icon'/> : undefined}
+        <input {...input} placeholder={label} type={type}>
+        </input>
+        {touched && error ? <span className="line-error"/> : <span className="line"/>}
+        {touched && error ? <label className='error-label'>{error}</label> : undefined}
+    </div>
+);
+
+const renderFieldRegistration = ({input, label, type, meta: {touched, error, warning}, placeHolder}) => (
+    <div>
+        <label>{placeHolder}</label>
+        {touched && error ? <Icon type='exclamation-circle' className='error-icon'/> : undefined}
+        <input {...input} placeholder={label} type={type}>
+        </input>
+        {touched && error ? <span className="line-error"/> : <span className="line"/>}
+        {touched && error ? <label className='error-label'>{error}</label> : undefined}
     </div>
 );
 
@@ -28,34 +39,105 @@ class AuthProvider extends React.Component {
 
     };
 
+    onSubmitRegistration = () => {
+
+    };
+
+    onChange(props) {
+    }
+
     loginForm() {
         const {handleSubmit} = this.props;
         return (
             <div>
-                <div className='user'>
+                <div>
                     <h2>
                         Вход на сайт
                     </h2>
+                    <div className='user'>
+                    </div>
                 </div>
                 <form onSubmit={handleSubmit(this.onSubmitLogin)}>
                     <Field
                         name='email'
                         type='text'
                         component={renderField}
-                        validate={[required, email]}
+                        validate={[required, email, maxLength]}
                         label="email"
                     />
                     <Field
                         name='password'
                         type='password'
                         component={renderField}
-                        validate={required}
+                        validate={[required, maxLength]}
                         label="Пароль"
                     />
                     <input type="submit" value="Войти" id="send"/>
                 </form>
+                <div className="forget-password">
+                    <a href="#">Забыли пароль?</a>
+                </div>
                 <div className='footer'>
-                    <Icon type='facebook' style={{fontSize:"18px", backgroundColor: "#3C599F",borderRadius: "50%"}}/>
+                    <a className='social-media'>
+                        <Icon type='google' className='icon-google-plus'>
+                            <span   />
+                        </Icon>
+                    </a>
+                </div>
+            </div>
+        )
+    }
+
+    registrationForm() {
+        const {handleSubmit} = this.props;
+        return (
+            <div>
+                <h2>
+                    Регистрация
+                </h2>
+                <div className='user'>
+                </div>
+                <form onSubmit={handleSubmit(this.onSubmitRegistration)}>
+                    <Field
+                        name='name'
+                        type='text'
+                        component={renderFieldRegistration}
+                        validate={[required, inputName, maxLength]}
+                        label="Введите Имя"
+                        placeHolder="Имя"
+                    />
+                    <Field
+                        name='secondname'
+                        type='text'
+                        component={renderFieldRegistration}
+                        validate={[inputName, maxLength]}
+                        label="Введите Фамилию"
+                    />
+                    <Field
+                        name='email'
+                        type='text'
+                        component={renderFieldRegistration}
+                        validate={[required, email, maxLength]}
+                        label="Введите E-mail"
+                    />
+                    <Field
+                        name='password'
+                        type='password'
+                        component={renderFieldRegistration}
+                        validate={[required, maxLength]}
+                        label="Пароль"
+                    />
+                    <Field
+                        name='confirmPassword'
+                        type='password'
+                        component={renderFieldRegistration}
+                        validate={required}
+                        label="Повторите пароль"
+                    />
+                    <input type="submit" value="Регистрация" id="send"/>
+                </form>
+                <div className='footer'>
+                    <Icon type='anticon-google'/>
                 </div>
             </div>
         )
@@ -65,11 +147,12 @@ class AuthProvider extends React.Component {
         return (
             <div>
                 <div className="log-form">
-                    <Tabs defaultActiveKey="1">
+                    <Tabs defaultActiveKey="1" onChange={this.onChange(this.props)}>
                         <TabPane tab="Авторизация" key="1">
                             {this.loginForm()}
                         </TabPane>
                         <TabPane tab="Регистрация" key="2">
+                            {this.registrationForm()}
                         </TabPane>
                     </Tabs>
                 </div>
--- webpack/src/app/components/forms/Validation.js
+++ webpack/src/app/components/forms/Validation.js
@@ -1,5 +1,13 @@
-
 export const required = values => values ? undefined : "Поле обязательно для заполнения";
 
 export const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
     ? 'Неверный формат' : undefined;
+
+export const login = value => value && !/^[a-zA-Z][a-zA-Z0-9_]*$/i.test(value)
+    ? 'Можно использовать только цифры и буквы латинского алфавита' : undefined;
+
+export const matLength = 15;
+export const maxLength = value => value && value.length > matLength ? "Максимальное количество симолов" : undefined;
+
+export const inputName = value => value && !/^[А-аЯ-я]|[A-aZ-z]*$/i.test(value)
+    ? "Можно использовать только буквы латинского и русские буквы" : undefined;
